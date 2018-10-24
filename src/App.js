ecffff1055835f818daa62e284ead4fa5ef2f2bb
Jose D Sanchez Frugone <jdsfrugone@gmail.com>
2018-10-23 18:47:36 -0700

    start of portfolio, navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+
+
+
 import './App.css';
 
 class App extends Component {
+
+  state = ({
+    header: {hover: false}
+  });
+
+  headerActive = () => {
+    this.setState({
+      header: {hover: true}
+    });
+  }
+
+  headerDeactive = () => {
+    this.setState({
+      header: {hover: false}
+    });
+  }
+
+
   render() {
+    const headerStyle = {
+      height : this.state.header.hover? "7%" : "3%"
+    }
+
+    const logoStyle = {
+      fontSize : this.state.header.hover? "2.5vw" : "1.1vw"
+    }
+
+    const anchorStyleLeft = {
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header style = {headerStyle}
+                onMouseEnter = {this.headerActive}
+                onMouseLeave = {this.headerDeactive}
+        >
+
+          <a className= "left-anchor" href = "#">  Home </a>
+          <a className = "left-anchor" href = "#">  About </a>
+          <a className = "left-anchor" href = "#"> Contact </a>
+          <h1 style = {logoStyle}> JDSF </h1>
+          <a className = "right-anchor" href = "#"> Proj 1 </a>
+          <a className = "right-anchor" href = "#"> Proj 2 </a>
+          <a className = "right-anchor" href = "#"> Proj 3 </a>
+
         </header>
       </div>
     );
