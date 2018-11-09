f12a389bf59edb6f0477a450a150d8d66c3c9d03
Jose D Sanchez Frugone <jdsfrugone@gmail.com>
2018-11-09 11:49:17 -0800

    portfolio styling + scrolling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Slide from 'react-reveal/Slide';
 import Carousel from './components/Carousel'
 
 
@@ -110,6 +111,7 @@ class App extends Component {
             </div>
             <div className = "circle circle-outline" id = "circle-04">
             </div>
+            <Slide right top > 
             <div id= "greeting-container">
               <div className = "circle" id = "circle-1">
               </div>
@@ -121,6 +123,7 @@ class App extends Component {
               </div>
               <h1>  hello,</h1>
             </div>
+            </Slide>
             <div id = "second-greeting">
               <div className = "circle" id = "circle-5">
               </div>
@@ -129,7 +132,6 @@ class App extends Component {
           </section>
           <section id = "about">
             <article>
-
               <div id = "about-greeting">
                 <div className = "circle" id = "circle-6">
                 </div>
@@ -141,15 +143,15 @@ class App extends Component {
                   <strong className = "pop-out">
                     Greetings!
                   </strong> My name is Jose, I'm a bioengineering graduate
-                  from UW Seattle exploring the world of web development and design.
+                  from UW Seattle exploring the world of web development and design
                   </span>
                 </p>
                 <p id= "second-blurb">
-                  In this humble abode you'll find some of the work I've produced
-                  during my trek. With all the different ways problems can be tackled
-                  I'd be hard pressed to say I'm not still learning the ropes.
-                  All in all I've been dabbling in javascript a little over six
-                  months and react for close to three.
+                  Here you'll find some of the work I've produced
+                  during my trek. Truth be told with all the different ways things
+                  can be done I'm really still learning the ropes.
+                  All in all I've been dabbling in javascript for about 7 months
+                  and react for close to 3
                 </p>
                 <p id = "third-blurb">
                     <span>
@@ -158,13 +160,12 @@ class App extends Component {
                       </strong>
                       I took some introductory OOP, statistical and engineering
                       programming during school but I didn't really become personally
-                      invested in the things I produced until my last quarter of school
+                      invested in the things I had created until my last quarter of school
                       when I took
-                      a web development class on a whim (we covered vanilla js, basic html
-                      CSS, JSON, AJAX, using APIs and serving content through PHP, as well as a little
-                      SQL). I still have a ton to learn about both front and
-                      back end but I am encouraged by what I've accomplished so far.
-                      Stay tuned!
+                      a web development class on a whim. We were strained for time
+                      but still managed to cover a little bit about vanilla js, basic HTML
+                      CSS, JSON, AJAX, serving and using APIs through PHP, as well as
+                      tiny bit of SQL. There's still so much more to learn!
                     </span>
                 </p>
                 <p id = "fourth-blurb">
@@ -174,11 +175,23 @@ class App extends Component {
                     </strong>
                     I was born in Santa Cruz,
                     Bolivia, a mid-sized landlocked country in the heart of South
-                    America home to ~10 million people. Personally I'm of the mindset
-                    that if folks genuinely care about something
-                    then that thing must have something to it, even if I don't particularly
-                    understand it at the moment. It's up to me to make up the difference
-                    and see things with the respect they deserve.
+                    America home to ~12 million people
+                  </span>
+                </p>
+                <p id = "fifth-blurb">
+                  <span>
+                    We sadly don't have the best infrastructure for tidy
+                    record-keeping—in fact, birth certificates were not a thing
+                    until 1940 and are still not a thing in some parts of the rural
+                    country—but Julia Flores Colque, a strong and humble Quechua
+                    woman, may be the world oldest
+                    person at 118 years of age. <strong>Take that</strong>,
+                    low human development index!
+                  </span>
+                </p>
+                <p id = "sixth-blurb">
+                  <span>
+                    Charitable understanding is an important thing in this world
                   </span>
                 </p>
               </div>
