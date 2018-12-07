455aff22fd8ef645f74fe28da400e99b449951f0
Jose D Sanchez Frugone <jdsfrugone@gmail.com>
2018-12-07 09:33:17 -0800

    Updates

---
+++ precache-manifest.fd4f6a1c6aefa6763a1b8b4f95697b81.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e98bcba53aee5cb8c89a952cd08f0ee0",
+    "url": "/static/media/zen.e98bcba5.png"
+  },
+  {
+    "revision": "61eb8b3b51264d02c1551cf7d1669bc1",
+    "url": "/static/media/nutri.61eb8b3b.png"
+  },
+  {
+    "revision": "a3f3b6748902d6717b977f46513f5382",
+    "url": "/static/media/notes.a3f3b674.png"
+  },
+  {
+    "revision": "c99a0650ee4754be054c23228843f657",
+    "url": "/static/media/mail.c99a0650.png"
+  },
+  {
+    "revision": "1832568e380f2beca75e00f5a76630cd",
+    "url": "/static/media/git.1832568e.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c814b2114f81ab6c8a4e",
+    "url": "/static/js/main.c814b211.chunk.js"
+  },
+  {
+    "revision": "97fcd96ca63e13a4534f",
+    "url": "/static/js/1.97fcd96c.chunk.js"
+  },
+  {
+    "revision": "c814b2114f81ab6c8a4e",
+    "url": "/static/css/main.4312c7aa.chunk.css"
+  },
+  {
+    "revision": "33b5cf323700a9db8f35c272a63281f4",
+    "url": "/index.html"
+  }
+];
